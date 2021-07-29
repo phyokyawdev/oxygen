c949a503cd5507aa2af5eb8761b5635d95ba0fdb
Phyo Kyaw <phyokyawdev@gmail.com>
2021-07-29 17:50:10 +0630

    update server setup

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const app = require("./src/app");
+const logger = require("./logger")("index");
 
-const port = process.env.PORT || 3000;
+const app = require("./src/app");
+const db = require("./setup/db");
+const env = require("./setup/env");
+
+process.on("uncaughtExceptionMonitor", (err) => {
+  logger.error("Uncaught Exception");
+  logger.error(err);
+});
+
+process.on("unhandledRejection", (err) => {
+  logger.error("Unhandled Rejection");
+  logger.error(err);
+  process.exit(1);
+});
+
+const env_variables = ["PORT", "MONGO_URI"];
 
 const start = async () => {
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  logger.info("Starting server...");
+
+  // check env variables
+  env.check(env_variables);
+
+  // connect to db
+  try {
+    await db.connect(process.env.MONGO_URI);
+  } catch (err) {
+    throw err;
+  }
+
+  // serve app
+  app.listen(process.env.PORT, () => {
+    logger.info(`Listening on port ${process.env.PORT}`);
   });
 };
 
--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
+const logger = require("../logger")("app");
 const express = require("express");
 const app = express();
+
+logger.debug("Preparing app...");
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+logger.debug("Preparing completed");
 module.exports = app;
