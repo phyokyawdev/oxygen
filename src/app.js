dbadb5e1e8dc59a5c1b718e169db3ccdd144af0c
Phyo Kyaw <phyokyawdev@gmail.com>
2021-07-30 15:28:26 +0630

    add error handler

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 const logger = require("../logger")("app");
 const express = require("express");
-const app = express();
+require("express-async-errors");
+
+const ClientError = require("./errors/client-error");
+const errorHandler = require("./middlewares/error-handler");
 
 logger.debug("Preparing app...");
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("*", async (req, res) => {
+  throw new ClientError(404, "Not found");
+});
+
+app.use(errorHandler);
 
 logger.debug("Preparing completed");
 module.exports = app;
---
+++ src/middlewares/error-handler.js
@@ -0,0 +1,21 @@
+const logger = require("../../logger")("error-handler");
+const ClientError = require("../errors/client-error");
+
+function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  if (err instanceof ClientError) {
+    return res.status(err.statusCode).send({
+      errors: err.serialize(),
+    });
+  }
+
+  logger.error(err);
+  res.status(500).send({
+    errors: [{ message: "Something went wrong!" }],
+  });
+}
+
+module.exports = errorHandler;
