67e2f0d7a466dbbbee675a67198d882e6481f3f7
Phyo Kyaw <phyokyawdev@gmail.com>
2021-08-02 21:07:16 +0630

    add auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,21 @@
+const passport = require("passport");
+const NotAuthorizedError = require("../errors/not-authorized-error");
+
+/**
+ * Attach user: { id: string, isAdmin?: boolean} to req if auth token is valid
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+function auth(req, res, next) {
+  passport.authenticate("jwt", { session: false }, (err, user, info) => {
+    if (err) return next(err);
+
+    if (!user) return next(new NotAuthorizedError(info.message));
+
+    req.user = user;
+    next();
+  })(req, res, next);
+}
+
+module.exports = auth;
